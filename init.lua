b56cfd0a68ceb68532ebb244091a8fd8e8f539fb
ChrinovicMu <chrinovicmu@gmail.com>
2025-03-19 12:19:08 +0200

    guicursor included

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
---vim.opt.guicursor = "n-v-c:block-,i-ci-ve:block-blockwait700-blinkoff400-blinkon250,r-cr:hor20,o:hor50"
+vim.opt.guicursor = "n-v-c:block-,i-ci-ve:block-blockwait700-blinkoff400-blinkon250,r-cr:hor20,o:hor50"
 vim.o.clipboard = "unnamedplus"
 vim.o.clipboard = "unnamedplus"
 vim.o.mouse = ""
